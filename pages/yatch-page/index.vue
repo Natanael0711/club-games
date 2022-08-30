2522edd0ed4670f3f83a57efdd63334da887de15
natanael <natanael.lima@catskillet.com>
2022-08-30 18:09:43 -0300

    feat: continuando criação da pagina de yatch

--- pages/yatch-page/index.vue
+++ pages/yatch-page/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <b-container>
-        <!-- <b-row>
+        <b-container>
+        <b-row>
             <b-col>
                 <button @click="checkRepeatedDices">Logar dados</button>
             </b-col>
@@ -9,72 +9,44 @@
             <b-col>
                 <YatchList :condition="4" :valueProp="dice1"/>
             </b-col>
-        </b-row> -->
+        </b-row>
         <b-row>
 
-            <b-col lg="4" v-for="(item, index) in [...Array(1).keys()]" :key="index">
+            <b-col lg="4" v-for="(item, index) in [...Array(5).keys()]" :key="index">
+                <!-- <DiceComponent 
+                    :active="turnOnOff"
+                    :diceFace="dice1"
+                    :on-change="(face) => {checkRepeatedDices(face)}"
+                />
+                {{ diceArray }} -->
                 <DiceComponent 
                     class="dice"
                     :active="turnOnOff"
                     :diceFace="dice1"
                     :on-change="(face) => {diceArray[round][index] = face}"
                 />
-            </b-col>
-            
-            <!-- <b-col lg="4" class="center-container">
-                <DiceComponent 
-                    class="dice"
-                    :active="turnOnOff"
-                    :diceFace="dice1"
-                    :on-change="(face) => {dice1 = face}"
-                />
-
-              
-            </b-col>
-            <b-col lg="4" class="center-container">
-                <DiceComponent 
-                    class="dice"
-                    :active="turnOnOff"
-                    :diceFace="dice1"
-                    :on-change="(face) => {dice2 = face}"
-                />
-
+                {{ diceArray }}
             </b-col>
 
-            <b-col lg="4" class="center-container">
-                <DiceComponent 
-                    class="dice"
-                    :active="turnOnOff"
-                    :diceFace="dice1"
-                    :on-change="(face) => {dice3 = face}"
-                />
-
-            </b-col>
-
-            <b-col cols="6" lg="6" class="center-container">
-                <DiceComponent 
-                    class="dice"
-                    :active="turnOnOff"
-                    :diceFace="dice1"
-                    :on-change="(face) => {dice4 = face}"
-                />
-
-            </b-col>
-
-            <b-col cols="6" lg="6" class="center-container">
-                <DiceComponent 
-                    class="dice"
-                    :active="turnOnOff"
-                    :diceFace="dice1"
-                    :on-change="(face) => {dice5 = face}"
-                />
-
-            </b-col> -->
         </b-row>
         <b-row>
             <b-col lg="12">
                 <button class="btn btn-lg btn-danger"
-                    @click="turnOnOff = !turnOnOff, timer(), showMatrix()"
+                    @click="showMatrix('Lets dye')"
+                >
+                    Log
+                </button>
+                <!-- <button class="btn btn-lg btn-danger"
+                    @click="showMatrix(diceArray[round][index])"
+                >
+                    Log
+                </button> -->
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col lg="12">
+                <button class="btn btn-lg btn-danger"
+                    @click="turnOnOff = !turnOnOff, timer()"
                 >
                     Play dice!
                 </button>
@@ -95,37 +67,42 @@
 
 <script>
 
-export default {
-    data(){
-        return {
-            dice1: null,
-            dice2: null,
-            dice3: null,
-            dice4: null,
-            dice5: null,
-            round: 0,
-            diceArray: [],
-            turnOnOff: false
-        }
-    },
-    methods: {
-        timer(){
-            this.time = 0;
-            setTimeout(() => {
-                this.time = 1
-            }, 3600)
+    export default {
+        data(){
+            return {
+                dice1: null,
+                dice2: null,
+                dice3: null,
+                dice4: null,
+                dice5: null,
+                round: 0,
+                diceArray: [],
+                turnOnOff: false
+            }
         },
-        checkRepeatedDices(){
-            const dices = document.querySelectorAll('.dice');
-            dices.forEach((dice) =>{
-                // console.log(dice.diceface);
-            })
-            // console.log(dices);
-        },
-        showMatrix(){
-            console.log(this.diceArray)
+        methods: {
+            timer(){
+                this.time = 0;
+                setTimeout(() => {
+                    this.time = 1
+                }, 3600)
+            },
+            // checkRepeatedDices(diceFace){
+            //     const dices = [];
+            //     dices.push(diceFace)
+                // dices.forEach((dice) =>{
+                //     console.log(dice);
+                // })
+            // },
+            checkRepeatedDices(){
+                const dices = document.querySelectorAll('.dice');
+                dices.forEach((dice) =>{
+                    console.log(dice);
+                })
+            },
+            showMatrix(hoi){
+                console.log(hoi)
+            }
         }
     }
-}
-
 </script>
